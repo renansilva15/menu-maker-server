ab208149a0fc2ff8a2426ba40a470e3e15a4a033
renansilva15 <renangabrielsilva150@gmail.com>
2023-05-23 17:11:59 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.listen({port: 3232}).then(() => {
+  console.log('HTTP server running on http://localhost:3232')
+})
+
+app.get('/', () => {
+  console.log('Test')
+})
