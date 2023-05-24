013b65ed1f67cff223b26ba4480ca5bb2b69850a
renansilva15 <renangabrielsilva150@gmail.com>
2023-05-24 13:00:14 -0300

    eslint config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({port: 3232}).then(() => {
+app.listen({ port: 3232 }).then(() => {
   console.log('HTTP server running on http://localhost:3232')
 })
 
