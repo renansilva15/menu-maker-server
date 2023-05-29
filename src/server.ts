b73022071322e391a19d16800add28d42385329a
renansilva15 <renangabrielsilva150@gmail.com>
2023-05-29 09:36:17 -0300

    prisma connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 app.listen({ port: 3232 }).then(() => {
   console.log('HTTP server running on http://localhost:3232')
 })
 
-app.get('/', () => {
-  console.log('Test')
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
