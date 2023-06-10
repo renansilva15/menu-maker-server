06b9400df63c05d718a7398931de3c30e60a602d
renansilva15 <renangabrielsilva150@gmail.com>
2023-06-10 17:47:25 -0300

    Products CRUD

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function productsRoutes(app: FastifyInstance) {
@@ -15,27 +16,84 @@ export async function productsRoutes(app: FastifyInstance) {
     return products
   })
 
-  app.get('/products/:id', async () => {
-    const users = await prisma.user.findMany()
+  app.get('/products/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-    return users
+    const { id } = paramsSchema.parse(request.params)
+
+    const product = await prisma.product.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return product
   })
 
-  app.post('/products', async () => {
-    const users = await prisma.user.findMany()
+  app.post('/products', async (request) => {
+    const bodySchema = z.object({
+      categoryId: z.string(),
+      nm: z.string(),
+      price: z.number(),
+    })
 
-    return users
+    const { categoryId, nm, price } = bodySchema.parse(request.body)
+
+    const product = await prisma.product.create({
+      data: {
+        managerId: '6aa91385-bb1a-4987-be18-5f7b90bacef9',
+        categoryId,
+        nm,
+        price,
+      },
+    })
+
+    return product
   })
 
-  app.put('/products/:id', async () => {
-    const users = await prisma.user.findMany()
+  app.put('/products/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-    return users
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      categoryId: z.string(),
+      nm: z.string(),
+      price: z.number(),
+    })
+
+    const { categoryId, nm, price } = bodySchema.parse(request.body)
+
+    const memory = await prisma.product.update({
+      where: {
+        id,
+      },
+
+      data: {
+        categoryId,
+        nm,
+        price,
+      },
+    })
+
+    return memory
   })
 
-  app.delete('/products/:id', async () => {
-    const users = await prisma.user.findMany()
+  app.delete('/products/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
 
-    return users
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.product.delete({
+      where: {
+        id,
+      },
+    })
   })
 }
